75721fa74edf680a535a163f6ff270a98baf09ff
NguyenXuanAnhThu <thutk08@gmail.com>
2016-10-19 23:55:57 +0700

    Create While01.c

---
+++ While01.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int a,b,k;
+    printf("HI BOSS\n");
+    printf("Moi nhap a, b\n");
+    scanf("%d%d",&a,&b);
+    while (k!=0)
+    {
+        k=a%b;
+        a=b;
+        b=k;
+    }
+      printf("uoc chung lon nhat=%d",a);
+
+}
